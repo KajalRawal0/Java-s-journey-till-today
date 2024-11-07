82e7087dd373cc78059dcb0f19e11282a4c72efb
Kajal Rawal <kajalrawal132@gmail.com>
2024-11-07 18:30:28 +0530

    Add files via upload

---
+++ primeNum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class primeNum {
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true; // If no divisors found, it's a prime number
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
